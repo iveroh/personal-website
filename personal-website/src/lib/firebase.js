a690322b0e65efaeffa5297697fb6cf26ffb1da6
Iver Heggelund <iveroh@stud.ntnu.no>
2025-03-24 09:24:19 +0100

    feat: :sparkles: Project template finished
    The project is now ready to be produced. Darkmode and lightmode implemented

--- personal-website/src/lib/firebase.js
+++ personal-website/src/lib/firebase.js
@@ -1,5 +1,6 @@
-// firebase.ts
+//firebase.js
 import { initializeApp } from "firebase/app";
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,7 +12,7 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-export default app;
+export { db };
